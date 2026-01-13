13c27b911ad6a14602d799c6922d7f2f1e8ea42e
Calvin Mwadime Makokha <mwadimemakokha@gmail.com>
2026-01-13 03:37:41 +0300

    feat(is_attacked): Check if a given square is being attacked by either side

--- bazuu.cxx
+++ bazuu.cxx
@@ -1,19 +1,14 @@
 #include "defs.hpp"
 #include <bazuu_ce_board.hpp>
 #include <memory>
+#include <print>
 
 int main() {
   std::unique_ptr<BazuuBoard> board = std::make_unique<BazuuBoard>();
-  board->setup_fen(KILLER_BOARD_FEN);
+  board->setup_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
   board->verify_all_magics();
-  auto bb = board->occupancy();
   board->print_board();
-  board->print_bit_board(board->get_knight_attacks(BoardSquares::E4));
-  board->print_bit_board(board->get_king_attacks(BoardSquares::E4));
-  board->print_bit_board(board->get_pawn_attacks(Colours::White, BoardSquares::E4));
-  board->print_bit_board(board->get_bishop_attacks_lookup(BoardSquares::E2, bb) &
-                         board->side_occupancy(Colours::Black));
-  board->print_bit_board(board->get_rook_attacks_lookup(BoardSquares::F1, bb));
-  board->print_bit_board(board->get_queen_attacks_lookup(BoardSquares::F3, bb) & board->side_occupancy(Colours::Black));
+  std::println("{}", board->is_square_attacked(BoardSquares::F6, Colours::White));
+  board->print_attacked_squares(Colours::Black);
   return 0;
 }
